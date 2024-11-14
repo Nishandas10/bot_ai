5baf1eed727db71d2427d33d10d5639f3e6f4a01
Nishant Das <dasbudhe@gmail.com>
2024-11-14 07:24:17 +0530

    building chatbot

---
+++ src/app/chatbot/page.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-empty-object-type */
+import AiChatBot from '@/components/chatbot'
+import React from 'react'
+
+type Props = {}
+
+const ChatBot = (props: Props) => {
+  return <AiChatBot />
+}
+
+export default ChatBot
