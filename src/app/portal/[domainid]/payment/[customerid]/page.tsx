1810d2c542c0f825a6b2a9e3ac3d5343c1c3f982
Nishant Das <dasbudhe@gmail.com>
2024-12-11 19:46:02 +0530

    stripe billing

--- src/app/portal/[domainid]/payment/[customerid]/page.tsx
+++ src/app/portal/[domainid]/payment/[customerid]/page.tsx
@@ -1,9 +1,37 @@
+/* eslint-disable @typescript-eslint/no-non-null-asserted-optional-chain */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import {
+  onDomainCustomerResponses,
+  onGetAllDomainBookings,
+} from '@/actions/appointment'
+import { onGetDomainProductsAndConnectedAccountId } from '@/actions/payments'
+import PortalForm from '@/components/forms/portal/portal-form'
 import React from 'react'
 
-const PaymentPage = () => {
+const CustomerPaymentPage = async ({
+  params,
+}: {
+  params: { domainid: string; customerid: string }
+}) => {
+  const questions = await onDomainCustomerResponses(params.customerid)
+  const products = await onGetDomainProductsAndConnectedAccountId(
+    params.domainid
+  )
+
+  if (!questions) return null
+
   return (
-    <div>PaymentPage</div>
+    <PortalForm
+      email={questions.email!}
+      products={products?.products}
+      amount={products?.amount}
+      domainid={params.domainid}
+      customerId={params.customerid}
+      questions={questions.questions}
+      stripeId={products?.stripeId!}
+      type="Payment"
+    />
   )
 }
 
-export default PaymentPage
+export default CustomerPaymentPage
--- src/components/settings/billing-settings.tsx
+++ src/components/settings/billing-settings.tsx
@@ -7,11 +7,13 @@ import { Card, CardContent, CardDescription } from '../ui/card'
 import { Check, CheckCircle2, Plus } from 'lucide-react'
 import { pricingCards } from '@/constants/landing-page'
 import Image from 'next/image'
+import Modal from '../modal'
+import SubscriptionForm from '../forms/settings/subscription-form'
 
 type Props = {}
 
 const BillingSettings = async (props: Props) => {
-  // WIP: Add Stripe subscription form
+
   const plan = await onGetSubscriptionPlan()
   const planFeatures = pricingCards.find(
     (card) => card.title.toUpperCase() === plan?.toUpperCase()
@@ -28,16 +30,33 @@ const BillingSettings = async (props: Props) => {
         />
       </div>
       <div className="lg:col-span-2 flex justify-start lg:justify-center ">
-        <Card className="border-dashed bg-cream border-gray-400 w-full cursor-pointer h-[270px] flex justify-center items-center">
-          <CardContent className="flex gap-2 items-center">
-            <div className="rounded-full border-2 p-1">
-              <Plus className="text-gray-400" />
-                </div>
-                <CardDescription className="font-semibold">
+      <Modal
+          title="Choose A Plan"
+          description="Tell us about yourself! What do you do? Let’s tailor your experience so it best suits you."
+          trigger={
+            plan && plan === 'STANDARD'? (
+              <Card className="border-dashed bg-cream border-gray-400 w-full cursor-pointer h-[270px] flex justify-center items-center">
+                <CardContent className="flex gap-2 items-center">
+                  <div className="rounded-full border-2 p-1">
+                    <Plus className="text-gray-400" />
+                  </div>
+                  <CardDescription className="font-semibold">
                     Upgrade Plan
-                </CardDescription>
+                  </CardDescription>
                 </CardContent>
-          </Card>
+              </Card>
+            ) : (
+              <Image
+                src="/images/creditcard.png"
+                width={400}
+                height={400}
+                alt="image"
+              />
+            )
+          }
+        >
+          <SubscriptionForm plan={plan!} />
+        </Modal>
       </div>
       <div className="lg:col-span-2">
         <h3 className="text-xl font-semibold mb-2">Current Plan</h3>
